317b97c49d7284668515b80f569af9f9da339c69
William Wong <w07wong@yahoo.com>
2019-11-21 01:10:13 -0800

    graph function

---
+++ graph.py
@@ -0,0 +1,7 @@
+import networkx as nx
+import matplotlib.pyplot as plt 
+graph = nx.fast_gnp_random_graph(50, 0.1, False)
+nx.draw(graph, with_labels=True) 
+plt.show() 
+matrix = nx.convert_matrix.to_numpy_matrix(graph)
+print(matrix)
