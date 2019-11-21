d6479a3a3a12caf93ce6ca12a40130d376e5eae4
David Paner <panerdavid@Davids-MacBook-Pro.local>
2019-11-20 22:56:46 -0800

    fuck you shawn

---
+++ rao.py
@@ -0,0 +1,41 @@
+import random
+
+random.seed(5)
+
+locations = []
+
+
+for i in range(1, 51):
+    locations.append("a" + str(i))
+
+#locations
+f = open('small.txt','w')
+
+#number of locations and homes
+f.write(str(50) + "\n")
+f.write(str(25) + "\n")
+
+for i in range(50):
+    f.write(locations[i] + " ")
+
+f.write("\n")
+
+#homes
+homes = []
+for i in range(25):
+    rand = random.sample(range(1, 50), 25)
+for i in rand:
+    homes.append(locations[i])
+    f.write(locations[i] + " ")
+
+f.write("\n")
+
+#starting location
+set1 = set(locations)
+set2 = set(homes)
+valid_starts = set1.difference(set2)
+
+
+f.write(valid_starts.pop())
+
+f.close()
