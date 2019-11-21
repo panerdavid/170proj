f92424bc8e5848f3d1c9a482f50068a445f900ae
David Paner <panerdavid@Davids-MacBook-Pro.local>
2019-11-20 23:17:34 -0800

    fuck

--- ramachandran.py
+++ ramachandran.py
@@ -5,25 +5,25 @@ random.seed(5)
 locations = []
 
 
-for i in range(1, 51):
+for i in range(1, 101):
     locations.append("a" + str(i))
 
 #locations
-f = open('small.txt','w')
+f = open('medium.txt','w')
 
 #number of locations and homes
+f.write(str(100) + "\n")
 f.write(str(50) + "\n")
-f.write(str(25) + "\n")
 
-for i in range(50):
+for i in range(100):
     f.write(locations[i] + " ")
 
 f.write("\n")
 
 #homes
 homes = []
-for i in range(25):
-    rand = random.sample(range(1, 50), 25)
+for i in range(50):
+    rand = random.sample(range(1, 100), 50)
 for i in rand:
     homes.append(locations[i])
     f.write(locations[i] + " ")
@@ -38,9 +38,8 @@ f.write(valid_starts.pop())
 
 f.write("\n")
 
-
-for i in range(50):
-    for j in range(50):
+for i in range(100):
+    for j in range(100):
         f.write("x ")
     f.write("\n")
 f.close()
